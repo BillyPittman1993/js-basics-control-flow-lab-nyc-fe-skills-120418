a3e7c719eb3e951239bd3a368ed7634e01afffe6
BillyPittman1993 <billytrading93@gmail.com>
2019-01-18 00:17:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function ternaryCheckCity() {
     result = 'Thank you.';
     break;
     }
-  case 'thanks for everything': {
+  default 'thanks for everything': {
     result = 'Bye.';
     break;
     }
